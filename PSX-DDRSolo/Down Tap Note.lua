2857be84c23847b4cb895ddff19a14928f9e6ea3
EngineMachiner <mfpc98@gmail.com>
2023-12-12 12:08:44 -0500

    Update Down Tap Note.lua

--- PSX-DDRSolo/Down Tap Note.lua
+++ PSX-DDRSolo/Down Tap Note.lua
@@ -5,7 +5,7 @@ return Def.ActorFrame{
 	
 	Def.Sprite{
 		Texture=NOTESKIN:GetPath( tex2, '30x16' ),
-		Frames = Sprite.LinearFrames( 60, 1 ),
+		Frames = Sprite.LinearFrames( 60, 2 ),
 		InitCommand=function(self)
 			local w = self:GetZoomedWidth()
 			local h = self:GetZoomedHeight()
