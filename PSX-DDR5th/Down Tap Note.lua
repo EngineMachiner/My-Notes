f8cb940111c858bfc12c7fda661f92bb401cfd39
EngineMachiner <mfpc98@gmail.com>
2023-12-12 12:08:26 -0500

    Update Down Tap Note.lua

--- PSX-DDR5th/Down Tap Note.lua
+++ PSX-DDR5th/Down Tap Note.lua
@@ -3,7 +3,7 @@ local tex2 = ... or "arrow"
 
 return Def.Sprite{
 	Texture=NOTESKIN:GetPath( tex2, '30x4' ),
-	Frames = Sprite.LinearFrames( 30*4, 1 ),
+	Frames = Sprite.LinearFrames( 30*4, 2 ),
 	InitCommand=function(self)
 		local w = self:GetZoomedWidth()
 		local h = self:GetZoomedHeight()
